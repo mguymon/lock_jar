40f4941ae52e5f84018f2841cbe8f4493f6b2a26
Darrick Wiebe <dw@xnlogic.com>
2014-11-18 15:58:56 -0500

    Allow multiple jarfiles to be registered, then merge and lock them all as one.
    This should allow for simple project-level jar dependencies as long as
    the call to LockJar.lock_registered_jarfiles can be deferred.

--- lib/lock_jar.rb
+++ lib/lock_jar.rb
@@ -99,6 +99,35 @@ module LockJar
   def self.read( lockfile )
     LockJar::Domain::Lockfile.read( lockfile )
   end
+
+  # Add a Jarfile to be eventually loaded together with other
+  def self.register_jarfile( jarfile )
+    fail "Jarfile not found: #{ jarfile }" unless File.exists? jarfile
+    @@registered_jarfiles ||= []
+    @@registered_jarfiles << jarfile
+  end
+
+  # Lock the registered Jarfiles and generate a Jarfile.lock.
+  #
+  # Options and groups are passed through to the LockJar.lock method, but
+  # if a jarfile is specified, it will be ignored. Use LockJar.register_jarfile
+  # to add dependencies.
+  #
+  # A block can be passed in, overriding values from the Jarfiles.
+  #
+  # @return [Hash] Lock data
+  def self.lock_registered_jarfiles( *args, &blk )
+    jarfiles = @@registered_jarfiles || []
+    instances = jarfiles.map do |jarfile|
+      LockJar::Domain::JarfileDsl.create jarfile
+    end
+    combined = instances.reduce do |result, inst|
+      LockJar::Domain::DslHelper.merge result, inst
+    end
+    args = args.reject { |arg| arg.is_a? String }
+    lock combined, *args, &blk
+  end
+
   private
 
   def self.extract_args(type, args, &blk )
