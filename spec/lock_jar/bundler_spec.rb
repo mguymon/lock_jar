5b9189f6e24c788a6ff314d2a55b9850c3743f4e
Michael Guymon <michael@tobedevoured.com>
2015-11-14 22:56:14 -0500

    additional spec

--- spec/lock_jar/bundler_spec.rb
+++ spec/lock_jar/bundler_spec.rb
@@ -4,14 +4,24 @@ require 'lock_jar/bundler'
 describe LockJar::Bundler do
   include Spec::Helpers
 
-  describe '.lock!' do
-    before do
-      remove_file('Jarfile.lock')
-      LockJar::Bundler.lock!('spec/fixtures/Jarfile')
-    end
+  before do
+    remove_file('Jarfile.lock')
+    LockJar::Bundler.lock!('spec/fixtures/Jarfile')
+  end
 
+  describe '.lock!' do
     it 'should create Jarfile.lock' do
       expect(File).to exist('Jarfile.lock')
+    end
+  end
+
+  describe '.load' do
+    it 'should load jars' do
+      LockJar::Bundler.load('test')
+
+      expected_jars = %w(junit:junit:jar:4.10 org.hamcrest:hamcrest-core:jar:1.1)
+
+      expect(LockJar::Registry.instance.loaded_jars).to eql(expected_jars)
     end
   end
 end
