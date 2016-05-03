d3b3321c392ea3f6b3440c66ef24f94e70d9c073
Michael Guymon <michael@tobedevoured.com>
2016-05-03 16:56:17 -0400

    stub Dir to prevent overwrite of local files

--- spec/lock_jar/config_spec.rb
+++ spec/lock_jar/config_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe LockJar::Config do
   describe '.load_config_file' do
     let(:test_config_file) { File.join('spec', 'fixtures', 'lock_jar_config.yml') }
+    let(:temp_config_file) { File.join(TEMP_DIR, described_class::DEFAULT_FILENAME) }
     let(:config) { described_class.load_config_file }
     let(:expected_repo_config) do
       {
@@ -13,9 +14,13 @@ describe LockJar::Config do
       }
     end
 
+    before do
+      FileUtils.cp(test_config_file, temp_config_file)
+    end
+
     context 'using current dir config' do
       before do
-        FileUtils.cp(test_config_file, File.join(Dir.pwd, described_class::DEFAULT_FILENAME))
+        allow(Dir).to receive(:pwd).and_return(TEMP_DIR)
       end
 
       it 'should have a repository config' do
@@ -24,9 +29,9 @@ describe LockJar::Config do
     end
 
     context 'using home dir config' do
+
       before do
-        FileUtils.rm(File.join(Dir.pwd, described_class::DEFAULT_FILENAME))
-        FileUtils.cp(test_config_file, File.join(Dir.home, described_class::DEFAULT_FILENAME))
+        allow(Dir).to receive(:home).and_return(TEMP_DIR)
       end
 
       it 'should have a repository config' do
@@ -36,7 +41,7 @@ describe LockJar::Config do
 
     context 'using ENV path to config' do
       before do
-        ENV[described_class::CONFIG_ENV] = File.join(Dir.pwd, described_class::DEFAULT_FILENAME)
+        ENV[described_class::CONFIG_ENV] = temp_config_file
       end
 
       it 'should have a repository config' do
